d9e8e50aae915e2ff770feef190707a2bd1d2024
Anh Duc <anhducams@gmail.com>
2024-07-26 12:15:00 +0700

    Add files via upload
    skibidi

---
+++ checkers/constants.py
@@ -0,0 +1,26 @@
+import pygame
+import os
+import sys
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+WIDTH, HEIGHT = 400, 400
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128,128,128)
+
+CROWN = pygame.transform.scale(pygame.image.load(resource_path('assets/crown.png')), (22, 12))
